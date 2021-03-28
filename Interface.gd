4291878a7ac99fa28580bae89d0c0a89a674d8db
gil <gino.ladowitch@gmail.com>
2021-03-28 21:11:46 +0200

    Simply update label text with user input.

--- Interface.gd
+++ Interface.gd
@@ -10,3 +10,7 @@ func _ready():
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 #func _process(delta):
 #	pass
+
+
+func _on_TextInput_text_entered(new_text):
+	$"VBoxContainer/Display".text = new_text
