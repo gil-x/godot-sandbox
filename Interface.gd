02673105c01217e6deeee1f7d10c2e3276c626b7
gil <gino.ladowitch@gmail.com>
2021-03-28 20:29:48 +0200

    Say hello.

--- Interface.gd
+++ Interface.gd
@@ -1,14 +1,10 @@
 extends Control
 
-
-# Declare member variables here. Examples:
-# var a = 2
-# var b = "text"
-
+var user_name = "Gil"
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
+	print("Hello {user_name}".format({"user_name": user_name}))
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
